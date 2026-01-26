43b9027708d6a034c3b9732a2a2a27bd0801695c
hymyq-ship-it <hymyq@metropolia.fi>
2026-01-26 10:18:39 +0200

    Change Pi estimation to fixed iterations
    Refactor code to use a fixed number of iterations for Monte Carlo simulation of Pi estimation.

--- 4.6.py
+++ 4.6.py
@@ -1,14 +1,16 @@
 import random
-N= int(input("Kuinka monta pistettä haluat:"))
 
 n=0
-lasku= 0
-while lasku <N:
+N=100000000
+i=0
+while i <N:
     x=random.uniform(-1,1)
     y=random.uniform(-1,1)
 
-    if x*x + y*y <1:
+    if x** 2 + y**2 <1:
         n=n+1
-        lasku=lasku+1
-arvo=4 * n/N
+    #seurava kierros
+    i=i+1
+#lasketaan piin likiarvo annetun kaavan avulla.
+arvo=(4 * n/N)
 print("piin likiarvo: ", arvo)
